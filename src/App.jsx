358af0bc81ec7314d777cfe97f6665eee0f199d0
Naqiuddin Kamil <naqiu923@gmail.com>
2023-08-09 19:40:56 +0800

    use module css

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import kamusStail from "./App.module.css";
+import stail from "./New.module.css";
+
+function App() {
+  return (
+    <>
+      <p className={[kamusStail.title]}>HelloReact</p>
+      <p className={[stail.title]}>HelloReact</p>
+
+    </>
+  );
+}
+
+export default App
